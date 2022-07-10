3bdb0f2014c6e7bd9cb0a003ba6aa03569492b0f
TsuRS <109035435+TsuRS@users.noreply.github.com>
2022-07-11 01:59:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function getSchedule() {
     const currentDate = new Date();
     const currentTime = getTime();
-    return (currentTime)
-    document.getElementById("schedule").innerHTML = output;
+    document.getElementById("schedule").innerHTML = currentTime;
 }
