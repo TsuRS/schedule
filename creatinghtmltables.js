477beaf9ac70895cd40ab92c274fb035dcbb99ef
TsuRS <109035435+TsuRS@users.noreply.github.com>
2022-07-11 22:58:03 +0900

    Add files via upload

---
+++ creatinghtmltables.js
@@ -0,0 +1,65 @@
+
+window.onload = function()
+{
+    //createTable1();
+
+    createTable2();
+
+    // createTable3();
+}
+
+function createTable2()
+{
+    const currentDate = new Date();
+    const anchorOff = 1
+    const anchorDate = new Date(2022, 6, 3, 0, 0, 0, 0);
+    const weekDifference = (currentDate.getTime() - anchorDate.getTime())/(1000*3600*24*7);
+    const weekOff = Math.floor((weekDifference + anchorOff) % 7);
+
+    const elements = Elements.GetElements();
+    const headings = Elements.GetHeadings();
+    const properties = Elements.GetProperties();
+
+    // create table
+    const table = document.createElement("table");
+
+    // create caption and add to table
+    const caption = document.createElement("caption");
+    const captiontext = document.createTextNode("IB 43K Schedule");
+    caption.appendChild(captiontext);
+    table.appendChild(caption);
+
+    // create row for headings...
+    const hrow = document.createElement("tr");
+    table.appendChild(hrow);
+
+    // ...and add cells to it
+    for(let heading of headings)
+    {
+        const th = document.createElement("th");
+        const thtext = document.createTextNode(heading);
+        th.appendChild(thtext);
+        hrow.appendChild(th);
+    }
+
+    // iterate data, adding rows and cells for each item
+    for(let element of elements)
+    {
+        if(element.week !== weekOff )
+        {
+        const drow = document.createElement("tr");
+        table.appendChild(drow);
+
+        for(let property of properties)
+        {
+            const td = document.createElement("td");
+            const tdtext = document.createTextNode(element[property]);
+            td.appendChild(tdtext);
+            drow.appendChild(td);
+        }
+    }
+    }
+
+    // add table to div
+    document.getElementById("tablediv").appendChild(table);
+}
---
+++ elements.js
@@ -0,0 +1,83 @@
+class Elements
+{
+    static GetHeadings()
+    {
+        return ["Week", "Mon", "Tue", "Wed", "Thu", "Fri"];
+    }
+
+    static GetProperties()
+    {
+        return ["week", "mon", "tue", "wed", "thu", "fri"];
+    }
+
+    static GetElements()
+    {
+        return [
+        {
+            week: 1,
+            mon: "数学 HL / 日本語 HL",
+            tue: "地歴",
+            wed: "化学 HL / 歴史 HL",
+            thu: "英語 SL/HL",
+            fri: "英語 SL/HL"
+        },
+        {
+            week: 2,
+            mon: "数学 HL / 日本語 HL",
+            tue: "物理 HL / 生物 HL",
+            wed: "日本語 SL/HL",
+            thu: "英語　SL/HL",
+            fri: "地歴"
+        },
+        {
+            week: 3,
+            mon: "ESS / 歴史 HL",
+            tue: "物理 HL / 生物 HL / 経済 HL",
+            wed: "日本語 SL/HL",
+            thu: "数学 SL/HL",
+            fri: "日本語 SL/HL"
+        },
+        {
+            week: 4,
+            mon: "ESS / 歴史 HL",
+            tue: "経済 SL/HL",
+            wed: "体育",
+            thu: "物理 SL/HL / 生物 SL/HL",
+            fri: "数学 SL/HL"
+        },
+        {
+            week: 5,
+            mon: "物理 HL / 生物 HL / 経済 HL",
+            tue: "Film SL / 化学 SL/HL",
+            wed: "英語 HL",
+            thu: "物理 SL/HL / 生物 SL/HL",
+            fri: "化学 HL / 歴史 HL"
+        },
+        {
+            week: 6,
+            mon: "体育",
+            tue: "Film SL / 化学 SL/HL",
+            wed: "TOK",
+            thu: "Film SL / 化学 SL/HL",
+            fri: "ESS / 歴史 HL"
+        },
+        {
+            week: 7,
+            mon: "LHR",
+            tue: "数学 SL/HL",
+            wed: "TOK",
+            thu: "体育",
+            fri: "経済 SL/HL"
+        },
+        {
+            week: "Extra",
+            mon: "",
+            tue: "英語 HL",
+            wed: "経済 SL/HL",
+            thu: "",
+            fri: ""
+        },
+
+    ]
+    }
+}
