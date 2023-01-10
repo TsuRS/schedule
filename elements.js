8bf0c20f9e04b2c866a7b3863435e432c6952b0f
TsuRS <109035435+TsuRS@users.noreply.github.com>
2023-01-10 11:10:50 +0900

    Update elements.js

--- elements.js
+++ elements.js
@@ -18,62 +18,62 @@ class Elements
             mon: "数学 HL / 日本語 HL",
             tue: "地歴",
             wed: "化学 HL / 歴史 HL",
-            thu: "英語 SL/HL",
-            fri: "英語 SL/HL"
+            thu: "英語 SL",
+            fri: "英語 SL"
         },
         {
             week: 2,
             mon: "数学 HL / 日本語 HL",
-            tue: "物理 HL / 生物 HL",
-            wed: "日本語 SL/HL",
-            thu: "英語　SL/HL",
+            tue: "物理 SL / 生物 SL",
+            wed: "日本語 SL",
+            thu: "英語　SL",
             fri: "地歴"
         },
         {
             week: 3,
             mon: "ESS / 歴史 HL",
             tue: "物理 HL / 生物 HL / 経済 HL",
-            wed: "日本語 SL/HL",
-            thu: "数学 SL/HL",
-            fri: "日本語 SL/HL"
+            wed: "日本語 SL",
+            thu: "数学 SL",
+            fri: "日本語 SL"
         },
         {
             week: 4,
             mon: "ESS / 歴史 HL",
-            tue: "経済 SL/HL",
+            tue: "経済 SL",
             wed: "体育",
-            thu: "物理 SL/HL / 生物 SL/HL",
-            fri: "数学 SL/HL"
+            thu: "物理 SL / 生物 SL",
+            fri: "数学 SL"
         },
         {
             week: 5,
             mon: "物理 HL / 生物 HL / 経済 HL",
-            tue: "Film SL / 化学 SL/HL",
+            tue: "Film SL / 化学 SL",
             wed: "英語 HL",
-            thu: "物理 SL/HL / 生物 SL/HL",
+            thu: "物理 SL / 生物 SL",
             fri: "化学 HL / 歴史 HL"
         },
         {
             week: 6,
             mon: "体育",
-            tue: "Film SL / 化学 SL/HL",
+            tue: "Film SL / 化学 SL",
             wed: "TOK",
-            thu: "Film SL / 化学 SL/HL",
+            thu: "Film SL / 化学 SL",
             fri: "ESS / 歴史 HL"
         },
         {
             week: 7,
             mon: "LHR",
-            tue: "数学 SL/HL",
+            tue: "数学 SL",
             wed: "TOK",
             thu: "体育",
-            fri: "経済 SL/HL"
+            fri: "経済 SL"
         },
         {
             week: "Extra",
             mon: "",
             tue: "英語 HL",
-            wed: "経済 SL/HL",
+            wed: "経済 SL",
             thu: "",
             fri: ""
         },
